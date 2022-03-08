6932a12996a2099a681f340b906a9858e8ec82f3
p1atdev <shu_teiei@outlook.jp>
2022-03-09 01:28:57 +0900

    initial commit

---
+++ app.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+const color = useColorMode({
+    modes: {
+        red: "red",
+        blue: "blue",
+    },
+})
+
+const toggleColor = () => {
+    color.value = color.value === "red" ? "blue" : "red"
+}
+</script>
+
+<template>
+    <main>
+        <button @click="toggleColor">Toggle Color</button>
+        <div>
+            <p>current: {{ color }}</p>
+
+            <div v-if="color == 'red'" class="onlyForRed">This is only for red</div>
+        </div>
+    </main>
+</template>
+
+<style>
+main {
+    height: 100vh;
+    margin: 0;
+    padding: 0;
+}
+
+html.red {
+    background: #eb2946;
+}
+html.blue {
+    background: #52b4f7;
+    color: #fafafa;
+}
+
+.onlyForRed {
+    font-size: xx-large;
+}
+</style>
